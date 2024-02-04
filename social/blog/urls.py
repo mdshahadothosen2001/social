db134c4d17f6ef329e9993cb79916d54f79ba566
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-02-04 15:02:58 +0600

    social

--- social/blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import PostList, CreatePostView
-
-urlpatterns = [
-    path('posts/', PostList.as_view(), name='post-list'),
-    path("create/", CreatePostView.as_view()),
-]
